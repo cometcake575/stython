768e115c2c35833ec7e16e9b2fc3bce25907bce0
cometcake575 <108220999+cometcake575@users.noreply.github.com>
2022-06-26 14:20:29 +0100

    Update setup.py
    Failed reupload, updating version to 1.2 to attempt to upload after deleting older files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='stython',
-    version='1.1',
+    version='1.2',
     license='MIT',
     author='Arun Kapila',
     author_email='starshootercity@gmail.com',
