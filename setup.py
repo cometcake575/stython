d986472a69fc1a5e932ddb31c74cd2d00ea97bb6
cometcake575 <108220999+cometcake575@users.noreply.github.com>
2022-06-26 12:41:37 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from stython import __version__
+
+setup(
+    name='stython',
+    version=__version__,
+
+    url='https://github.com/cometcake575/stython',
+    author='Arun Kapila',
+    author_email='starshootercity@gmail.com',
+
+    py_modules=['stython'],
+)
