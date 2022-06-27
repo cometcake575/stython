0be52df233bf93a207be45a14c4a3a7486b61976
cometcake575 <108220999+cometcake575@users.noreply.github.com>
2022-06-27 18:34:15 +0100

    Update setup.py
    Try to find all packages

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='stython',
-    version='1.6',
+    version='1.7',
     license='MIT',
     author='Arun Kapila',
     author_email='starshootercity@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'stython'},
+    packages=find_packages(),
     url='https://github.com/cometcake575/stython',
     install_requires=[],
 
