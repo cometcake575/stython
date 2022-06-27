bd2f99f2315ed61045eb90b7d36d1ecd46eee699
cometcake575 <108220999+cometcake575@users.noreply.github.com>
2022-06-27 18:26:17 +0100

    Update setup.py
    Version 1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stython',
-    version='1.5',
+    version='1.6',
     license='MIT',
     author='Arun Kapila',
     author_email='starshootercity@gmail.com',
